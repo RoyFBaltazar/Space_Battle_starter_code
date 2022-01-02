cfaf9ffd7e092c920642bf97a8e97d5768509c73
RoyFBaltazar <94885352+RoyFBaltazar@users.noreply.github.com>
2022-01-01 23:08:30 -0500

    it works

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ class Earth{
       this.accuracy = .7;
     }
     attack(enemy){
-     if (Math.random() < this.accuracy){
+      let ranNum = Math.random()
+     if (ranNum < this.accuracy){
+       console.log("hit")
       enemy.hull -= this.firepower;
-    alert("hit")}
+    }
        if(enemy.hull <= 0){
-        console.log(`${enemy.ship} is dead`)}
+        console.log(`${enemy.alien_ship} is dead`)}
         else 
         {console.log("missed")}
         }
@@ -33,10 +35,12 @@ class Alien{
        this.accuracy = (Math.floor(Math.random()*2) + 6)/10;
    }
     
-    attack(enemy){
-     if (Math.random() < this.accuracy){
-      enemy.hull -= this.firepower}
-       if(enemy.hull<=0){
+   attack(enemy){
+      let ranNum = Math.random()
+     if (ranNum < this.accuracy){
+      enemy.hull -= this.firepower;
+    }
+       if(enemy.hull <= 0){
         console.log(`${enemy.ship} is dead`)}
         else 
         {console.log("missed")}
@@ -44,50 +48,45 @@ class Alien{
 }
 const enemyImgs = document.getElementById("enemyimage")
 let earthShips = new Earth('earth')
-let alienShipsArr = []
-// function to create aliens 
-for (let i=0; i < 6; i++){
- let newAliens = new Alien()
-//  const newAlienImg = document.createElement('img');
-//  newAlienImg.setAttribute('src', "/images/enemy.gif")
-//  enemyImgs.appendChild(newAlienImg)
-  alienShipsArr.push(newAliens)
+let alienShipsArr = [new Alien('E.T'), new Alien('Paul'), new Alien('Megamind'), new Alien('Yoda'), new Alien('grogu'), new Alien('jr')]
  
- }
+// function to create aliens 
+// for (let i=0; i < 6; i++){
+//  let newAliens = new Alien()
+// //  const newAlienImg = document.createElement('img');
+// //  newAlienImg.setAttribute('src', "/images/enemy.gif")
+// //  enemyImgs.appendChild(newAlienImg)
+//   alienShipsArr.push(newAliens)
+ 
+//  }
 let battle = () => {
- while (alienShipsArr.length > 0){
-   if (earthShips.hull > 0){
-     if (Math.random() < .7){
-       earthShips.attack(alienShipsArr)
-      
-     }
-       }
-       else{
-           alert("missed")
-           console.log("try again ")
-         }
-         console.log(`${earthShips.hull} ${alienShipsArr.hull}`)
-     }
-     if (alienShipsArr.hull > 0){
-       alienShipsArr.attack(earthShips)}
-       
-       else  {
-         alienShipsArr.shift()
-         if (alienShipsArr.length === 0){console.log(alienShipsArr.length)}}
+    while(alienShipsArr.length > 0 || earthShips.length > 0){
+      if(earthShips.hull > 0){
+      earthShips.attack(alienShipsArr[0])}
+     
+      else{
+        console.log("retreat")
+        }
+        if (alienShipsArr[0].hull > 0){
+          alienShipsArr[0].attack(earthShips)
+          console.log("alien attack")}
+           if(alienShipsArr[0].hull <= 0){
+        alienShipsArr.shift()}
+      }
+      console.log(alienShipsArr.length)
          let attack_retreat = prompt(`your health is ${earthShips.hull}, attack or retreat`)
          if (attack_retreat=== "attack"){
             earthShips.attack(alienShipsArr) }
            else {
             alert("over")
             }
+            if (alienShipsArr.length === 0){
+                console.log("you Won")
+            }
    }
-//    alert("click anywhere to start")
-//    document.addEventListener("click",readyToPlay())
-//    const myTimeout = setTimeout(readyToPlay(), 20000)
 
 
-
-alert("slide your mouse over t0 the Enemy Image to start")
+alert("slide your mouse over the Enemy Image to start,and Open your console!")
 function readyToPlay (){
     let play = prompt("Do you want to play?")
   if(play === "yes"){
