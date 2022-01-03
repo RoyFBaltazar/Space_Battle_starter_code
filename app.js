2f1ac4d3a9be09b71bfb1fb6639b7ddcb6337a1a
RoyFBaltazar <94885352+RoyFBaltazar@users.noreply.github.com>
2022-01-03 09:18:31 -0500

    lasts updates

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-//  alert("click anywhere to start")
-//  document.addEventListener('click',readyToPlay())
-//  function readyToPlay (){
-// let play = prompt("Do you want to play?")
-// if(play === "yes"){
-//     battle()
-// } else{
-//     console.log("are you sure?")
-// }
-//  }
+
 class Earth{
     constructor(ship){
       this.ship = ship;
@@ -16,12 +7,14 @@ class Earth{
       this.accuracy = .7;
     }
     attack(enemy){
+        //generates random number to determine attack or miss
       let ranNum = Math.random()
      if (ranNum < this.accuracy){
        console.log("hit")
       enemy.hull -= this.firepower;
     }
-       if(enemy.hull <= 0){
+    // when alien ship dies its name is logged   
+    if(enemy.hull <= 0){
         console.log(`${enemy.alien_ship} is dead`)}
         else 
         {console.log("missed")}
@@ -30,9 +23,9 @@ class Earth{
 class Alien{
    constructor(alienship){
       this.alien_ship = alienship;
-       this.hull = Math.floor(Math.random()*6)+3;
+       this.hull = Math.floor(Math.random()*4)+3;
        this.firepower = Math.floor(Math.random()*3)+2;
-       this.accuracy = (Math.floor(Math.random()*2) + 6)/10;
+       this.accuracy = (Math.floor(Math.random()*3) + 6)/10;
    }
     
    attack(enemy){
@@ -49,7 +42,8 @@ class Alien{
 const enemyImgs = document.getElementById("enemyimage")
 let earthShips = new Earth('earth')
 let alienShipsArr = [new Alien('E.T'), new Alien('Paul'), new Alien('Megamind'), new Alien('Yoda'), new Alien('grogu'), new Alien('jr')]
- 
+const playerStat = document.getElementById("playerStats")
+const enemyStat = document.getElementById("enemyStat")
 // function to create aliens 
 // for (let i=0; i < 6; i++){
 //  let newAliens = new Alien()
@@ -60,15 +54,25 @@ let alienShipsArr = [new Alien('E.T'), new Alien('Paul'), new Alien('Megamind'),
  
 //  }
 let battle = () => {
-    while(alienShipsArr.length > 0 || earthShips.length > 0){
-      if(earthShips.hull > 0){
-      earthShips.attack(alienShipsArr[0])}
+    // //updates hull on html
+    // playerStat.textContent = `${earthShips.hull}`
+    // while loop for battle 
+    while(alienShipsArr.length > 0 ){
+        if(earthShips.hull > 0){
+      earthShips.attack(alienShipsArr[0])
+      playerStat.textContent = `Hull:${earthShips.hull}`
+      enemyStat.textContent = `Hull: ${alienShipsArr.hull}`}
      
       else{
         console.log("retreat")
         }
-        if (alienShipsArr[0].hull > 0){
+        if (alienShipsArr[0].hull > 0 ){
           alienShipsArr[0].attack(earthShips)
+          playerStat.textContent = `${earthShips.hull}`
+          enemyStat.textContent = `Hull: ${alienShipsArr[0]}`
+          if(earthShips.hull < 0){
+              console.log("game over")
+          }
           console.log("alien attack")}
            if(alienShipsArr[0].hull <= 0){
         alienShipsArr.shift()}
@@ -76,17 +80,19 @@ let battle = () => {
       console.log(alienShipsArr.length)
          let attack_retreat = prompt(`your health is ${earthShips.hull}, attack or retreat`)
          if (attack_retreat=== "attack"){
-            earthShips.attack(alienShipsArr) }
+            earthShips.attack(alienShipsArr)
+            enemyStat.textContent = `Hull: ${alienShipsArr[0]}` }
            else {
             alert("over")
             }
             if (alienShipsArr.length === 0){
+                alert("Earth Continues, We won")
                 console.log("you Won")
             }
    }
 
 
-alert("slide your mouse over the Enemy Image to start,and Open your console!")
+alert("Slide your mouse over the Enemy Image to start! Open your console!")
 function readyToPlay (){
     let play = prompt("Do you want to play?")
   if(play === "yes"){
